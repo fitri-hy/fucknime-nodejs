5bac2bf86e5955f7dd24648d4ca0b52cb2f19ae5
fitri-hy <noreply.orzpartners@gmail.com>
2025-07-16 17:41:20 +0700

    fix(detail): remove unnecessary https protocol from image URL

--- controllers/AnimeAPI/Detail.js
+++ controllers/AnimeAPI/Detail.js
@@ -12,7 +12,7 @@ exports.index = async (req, res) => {
     const $ = cheerio.load(data);
 
     const title = $('.infox h1[itemprop="headline"]').text().trim();
-    const imageUrl = `https:${$('.thumb img').attr('src')}`;
+    const imageUrl = `${$('.thumb img').attr('src')}`;
     const description = $('.sepele').text().trim().replace(/Animasu/gi, 'FuckNime') || "-";
     const status = $('.spe span:contains("Status:") font').text().trim() || "-";
     const releaseDate = $('.spe span.split:contains("Rilis:")').text().replace('Rilis:', '').trim() || "-";
